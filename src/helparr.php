8da4a0fecebc3b0b5717cc846bda85b532879bc3
Laurent DE QUAERIS <laurent@dequaeris.fr>
2022-02-22 23:33:03 +0100

    Add UX

--- src/helparr.php
+++ src/helparr.php
@@ -1,17 +1,32 @@
 <?php
 
-function p($array, $dump=false) {
+<?php
 
-	echo '<pre>';
-	print_r($array);
-	echo '</pre>';
+function p($array) {
+
+
+
+}
+
+
+
+
+function p($array, $dump=false) {
 
 	if($dump==true) {
 
-		echo '<hr><pre>';
+		echo '<style type="text/css">.hide{display: none</style><b>'.date("Y-m-d H:i:s").'</b><br><pre style="padding:2rem;background:#EFEFEF;cursor:pointer" onclick="javascript:document.getElementById(\'details\').classList.toggle(\'hide\')">';
+		print_r($array);
+		echo '</pre><hr><pre id="details" class="hide">';
 		var_dump(debug_backtrace());
 		echo '</pre>';
 
+	} else {
+
+		echo '<pre>';
+		print_r($array);
+		echo '</pre>';
+		
 	}
 
 }
